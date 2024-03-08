1e3b69039eb8b00d8246d4842061ccfcd3c5802a
Ankit Yadav <khushhal2112@gmail.com>
2024-03-08 10:49:13 +0000

    container config done

--- basics.py
+++ basics.py
@@ -1,33 +1,33 @@
-# name = "Ankit"
-# age = 22.0
-# print(name)
-# print(age)
-# is_adult = False
-# print("Hello world")
+name = "Ankit"
+age = 22.0
+print(name)
+print(age)
+is_adult = False
+print("Hello world")
 
-# first_name = "Ankit" 
-# last_name = "Yadav"
-# age = 19
-# is_genius = True
+first_name = "Ankit" 
+last_name = "Yadav"
+age = 19
+is_genius = True
 
 
-# input in python
-# names = input("what is your name")
-# print("Hello " + name)
+input in python
+names = input("what is your name")
+print("Hello " + name)
 
-# superHero = input("name your superhero")
-# print("my superHero is :" + superHero)
+superHero = input("name your superhero")
+print("my superHero is :" + superHero)
 
-# #type conversion
-# old_age = input("enter your age")
-# new_age = int(old_age) + 2
-# print(new_age)
+#type conversion
+old_age = input("enter your age")
+new_age = int(old_age) + 2
+print(new_age)
 
-# first = input("enter your first number")
-# second = input("enter your second number")
+first = input("enter your first number")
+second = input("enter your second number")
 
-# sum = int(first)+int(second)
-# print("the sum is : " + str(sum))
+sum = int(first)+int(second)
+print("the sum is : " + str(sum))
 
 #methods in string 
 name = "ankit yadav"
