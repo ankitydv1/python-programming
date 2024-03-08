a9ed6443ff29f10135b30ed92a524c8093380cd0
Ankit Yadav <khushhal2112@gmail.com>
2024-03-08 10:19:16 +0000

    container config done

---
+++ basics.py
@@ -0,0 +1,36 @@
+# name = "Ankit"
+# age = 22.0
+# print(name)
+# print(age)
+# is_adult = False
+# print("Hello world")
+
+# first_name = "Ankit" 
+# last_name = "Yadav"
+# age = 19
+# is_genius = True
+
+
+# input in python
+# names = input("what is your name")
+# print("Hello " + name)
+
+# superHero = input("name your superhero")
+# print("my superHero is :" + superHero)
+
+# #type conversion
+# old_age = input("enter your age")
+# new_age = int(old_age) + 2
+# print(new_age)
+
+# first = input("enter your first number")
+# second = input("enter your second number")
+
+# sum = int(first)+int(second)
+# print("the sum is : " + str(sum))
+
+#methods in string 
+name = "ankit yadav"
+print(name.upper())
+print(name)
+
